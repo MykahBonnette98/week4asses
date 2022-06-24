d7f4f7fe65d02480751b1f7d70d6b78822322566
Mykah Bonnette <mykahwalker@Chances-Air.localdomain>
2022-06-24 17:03:04 -0600

    resubmit

--- client/main.js
+++ client/main.js
@@ -19,6 +19,23 @@ const getFortune = () => {
     })
 }
 
+function search_char() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('characters');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
+
+
 complimentBtn.addEventListener('click', getCompliment)
 fortuneBtn.addEventListener('click', getFortune)
+
 
--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,8 @@
 module.exports = {
     getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+        const compliments = ["Gee, you're a smart cookie!", 
+        "Cool shirt!", 
+        "Your Javascript skills are stellar."];
       
         // choose random compliment
         let randomIndex = Math.floor(Math.random() * compliments.length);
@@ -10,7 +12,11 @@ module.exports = {
     },
 
     getFortune: (req, res) => {
-        const fortune = ["A lifetime of happiness lies ahead of you.", "All your hard work will soon pay off.", "Better ask twice than lose yourself once", "Fearless courage is the foundation of victory.", "How you look depends on where you go."];
+        const fortune = ["A lifetime of happiness lies ahead of you.",
+         "All your hard work will soon pay off.", 
+         "Better ask twice than lose yourself once",
+          "Fearless courage is the foundation of victory.",
+           "How you look depends on where you go."];
   
     let randomIndex = Math.floor(Math.random() * fortune.length);
     let randomFortune = fortune[randomIndex];
